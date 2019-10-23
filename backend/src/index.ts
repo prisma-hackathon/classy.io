fc447cece5d6fe3ae5f8985e8aa91d2a47344225
Divyendu Singh <singh@prisma.io>
2019-10-23 16:03:30 +0200

    yak shaving

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,37 @@
-const { ApolloServer, gql } = require('apollo-server')
+import { ApolloServer, gql } from 'apollo-server'
+import AWS from 'aws-sdk'
+import dotenv from 'dotenv'
+import crypto from 'crypto'
+
+import { Photon } from '@generated/photon'
+
+dotenv.config()
+
+function getRandomString(): string {
+  return crypto.randomBytes(20).toString('hex')
+}
+
+function extractObjectUrl(url: string): string {
+  const parsed = new URL(url)
+  parsed.search = ''
+  return parsed.toString()
+}
+
+const s3 = new AWS.S3({
+  region: 'eu-central-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
+
+const photon = new Photon()
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    health: String
   }
   type Mutation {
     upload: String
-    uploadDone: String
+    uploadDone(id: Int): String
   }
 `
 
@@ -15,8 +40,34 @@ const resolvers = {
     health: () => '200 OK',
   },
   Mutation: {
-    upload: () => {},
-    uploadDone: () => {},
+    upload: async () => {
+      const filename = `${getRandomString()}.zip`
+      const signedUrl = await s3.getSignedUrlPromise('putObject', {
+        Bucket: process.env.S3_BUCKET,
+        Key: filename,
+      })
+      const zipUrl = extractObjectUrl(signedUrl)
+
+      const job = await photon.classificationJobs.create({
+        data: {
+          signedUrl,
+          zipUrl,
+          zipUploaded: false,
+        },
+      })
+      return {
+        id: job.id,
+        signedUrl,
+      }
+    },
+    uploadDone: async (id: number) => {
+      const data = await photon.classificationJobs.update({
+        where: {
+          id,
+        },
+        data: {},
+      })
+    },
   },
 }
 
